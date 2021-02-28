7b86bb0ccdf05755351447c0a60c198c77053a29
sami-htw <community4coding@gmail.com>
2021-02-28 18:26:34 +0100

    TicTacToe Example

---
+++ src/TicTacToe/TicTacToeTest.java
@@ -0,0 +1,34 @@
+package TicTacToe;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+
+class TicTacToeTest {
+
+  TicTacToe object ;
+
+  @Before
+  public void init(){
+    object= new TicTacToe();
+  }
+
+  @org.junit.jupiter.api.Test
+  void main() {
+
+  }
+
+  @org.junit.jupiter.api.Test
+  void printGameBoard() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void boardChoices() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void checkWinner() {
+
+    object= new TicTacToe();
+
+  }
+}
